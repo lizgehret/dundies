dd719b0626d752d65ce0dc781120b54a8e7abc54
Liz Gehret <elizabeth.gehret@gmail.com>
2021-12-22 15:41:06 -0800

    imp: refactor to cycle through quotes from a requested character

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@ extern crate rand;
 
 use rand::seq::IteratorRandom;
 use std::fs;
+use std::env;
 pub fn main() {
+    // TODO: print welcome message before prompting user input
+    // println!("Welcome to the annual Dundie Awards Ceremony!
+    // Please type the name of the Office character you'd like to see a quote from.\n");
+
+    let args: Vec<String> = env::args().collect();
+    let char = &args[1];
+
+    let path = "./dundies/".to_owned() + char;
 
     let mut rng = rand::thread_rng();
 
-    let files = fs::read_dir("./dundies").unwrap();
+    let files = fs::read_dir(path).unwrap();
     let file = files.choose(&mut rng).unwrap().unwrap();
     let fp = file.path();
-
-    println!("{:?}", fp);
 
     let quote = fs::read_to_string(fp)
     .expect("Something went wrong reading the file.");
